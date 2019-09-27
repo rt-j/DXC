fa2f2180e7a7dce0af958fa1eb33dd435466489e
rt-j <55374365+rt-j@users.noreply.github.com>
2019-09-27 07:52:04 +0530

    Update Leap.java

--- Tests/Leap.java
+++ Tests/Leap.java
@@ -5,21 +5,21 @@ public class Leap {
 	public void year(){
 		int year=0;
 		int f=0;
-		int h=0;
+		
 		System.out.println("Enter the year:");
 		year=sc.nextInt();
 		if(year<=400) {
 			f=(year%4==0)?1:0;
 		}
 		else if(year>400){
-		h=(year%400==0)?1:0;
+		f=((year%400==0) ||(year%4==0))?1:0;
 		}
 		else {
 			f=0;
-			h=0;
+		
 		}
 		
-	if(f==1 || h==1) {
+	if(f==1) {
 			System.out.println("Leap year");
 		}
 		else {
